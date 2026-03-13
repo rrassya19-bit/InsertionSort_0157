386a526f064985d0e5795b81ad27b5dc63f9ad6f
rrassya19-bit <rrassys19@gmail.com>
2026-03-13 14:27:26 +0700

    menambahkan prosedur input

--- InsertionSort_0157.cpp
+++ InsertionSort_0157.cpp
@@ -3,3 +3,26 @@ using namespace std;
 
 int arr [20];
 int n;
+
+void input(){
+    while (true){
+        cout << "Masukan Jumlah Data Pada array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "==========================================";
+    cout << "===========Masukan Elemen Array===========" << endl; 
+    cout << "==========================================";
+
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
